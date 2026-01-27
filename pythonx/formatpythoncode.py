04013cf7f0818e10b6400cc4217494add6dacbde
Yiyang Wu <xgreenlandforwyy@gmail.com>
2026-01-27 17:23:13 +0800

    Fix ipython 8 indentation issue: add backspace like version <7.1
    MWE:
    
    def a(b):
        if b > 0:
            return 10
        else :
            return 5
    
    Similar-to: https://github.com/sillybun/vim-repl/issues/156

--- pythonx/formatpythoncode.py
+++ pythonx/formatpythoncode.py
@@ -266,7 +266,7 @@ class pythoncodes:
             elif self.replprogram == "ipython":
                 version = Version(self.version)
                 # print(f'{version=}, {self.version=}')
-                if version >= Version('7.1'):
+                if version >= Version('7.1') or version <= Version('8.0'):
                     return False
                 if line.startswith("pass ") or line.startswith("return ") or line.startswith("raise ") or line.startswith("continue ") or line.startswith("break "):
                     return True
